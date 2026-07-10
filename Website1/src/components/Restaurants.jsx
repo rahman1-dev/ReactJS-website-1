f9c9a2f8292010592ee951417a075fc8a99327a3
Abdur Rahman <ar8058276@gmail.com>
2026-07-09 23:40:05 -0500

    Fetching actual swiggy.com data

--- Website1/src/components/Restaurants.jsx
+++ Website1/src/components/Restaurants.jsx
@@ -1,23 +1,34 @@
 import ResCards from "./ResCards";
 import { RestaurantsDetails } from "../utils/RestaurantsDetails";
 import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 
 const Restaurants = () => {
-  const fethData = async () => {
+  let [hotelList, setHotelList] = useState([]);
+
+  const fetchApiData = async () => {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.38430&lng=78.45830&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7038628&lng=77.1023104&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(
+    //   data.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+    // );
+    setHotelList(
+      data.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+    );
   };
-  fethData();
+
+  useEffect(() => {
+    fethApiData();
+  }, []);
 
   return (
     <div className="restaurant">
       <div className="rest-cards-container">
         {/* <Shimmer /> */}
-        {RestaurantsDetails.map((res) => {
-          return <ResCards resData={res} key={res.id} />;
+        {hotelList.map((res) => {
+          return <ResCards resData={res.info} key={res.info.id} />;
         })}
       </div>
     </div>
